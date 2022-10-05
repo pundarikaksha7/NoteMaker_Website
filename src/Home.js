7237b1b0a9247706201ffd5292859e5838fc720a
pundarikaksha7 <98964611+pundarikaksha7@users.noreply.github.com>
2022-10-05 22:05:37 +0530

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
+import { Link } from 'react-router-dom';
 import BlogList from './BlogList';
 import useFetch from './useFetch';
+
+
 const Homepage = () => {
     const {data:blogs,isLoading,errormessage}=useFetch("http://localhost:8000/blogs");
     return ( 
 
-        <div className="home">
+        <div className="home" >
             {errormessage && <div>{errormessage}</div>}
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Notes" />}
+            {blogs && (<Link to="/create">
+                <button>Add Note +</button>
+            </Link>)}
+            
             
         </div>
      );
